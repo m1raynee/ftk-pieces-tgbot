25de7b61373e67cd353b07e54f8e6a59d0a2c963
miray <71838742+m1raynee@users.noreply.github.com>
2025-12-20 15:55:21 +0300

    shuffle logic around

--- pieces-bot/src/main/java/com/m1raynee/db/DataBase.java
+++ pieces-bot/src/main/java/com/m1raynee/db/DataBase.java
@@ -1,5 +1,12 @@
 package com.m1raynee.db;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.m1raynee.db.dto.PieceAmountDTO;
+import com.m1raynee.db.entity.Box;
+import com.m1raynee.db.entity.Piece;
 import com.m1raynee.db.entity.Student;
 
 public final class Database {
@@ -10,5 +17,34 @@ public final class Database {
         Student student = new Student(name);
         HibernateConfiguration.getSessionFactory().inTransaction(session -> session.persist(student));
         return student;
+    }
+
+    public static Box fetchBoxPreview(Integer box_index) {
+        return HibernateConfiguration.getSessionFactory().fromSession(session -> {
+            List<PieceAmountDTO> amounts = session.createSelectionQuery("""
+                    select new com.m1raynee.db.dto.PieceAmountDTO(p.id, SUM(pa.amount))
+                    from Piece p
+                    left join p.actions pa
+                    join p.box b
+                    where b.index=:index
+                    group by p.id
+                    """,
+                    PieceAmountDTO.class)
+                    .setParameter("index", box_index)
+                    .getResultList();
+
+            Map<Long, Long> stockMap = amounts.stream()
+                    .collect(Collectors.toMap(PieceAmountDTO::getPieceId,
+                            dto -> dto.getCurrentAmount() != null ? dto.getCurrentAmount() : 0L));
+
+            Box resultBox = session
+                    .createSelectionQuery("from Box b left join fetch b.pieces where b.index=:index", Box.class)
+                    .setParameter("index", box_index).getSingleResultOrNull();
+
+            for (Piece piece : resultBox.getPieces()) {
+                piece.setCalculatedAmount(stockMap.get(piece.getId()));
+            }
+            return resultBox;
+        });
     }
 }
--- pieces-bot/src/main/java/com/m1raynee/telegram/handlers/BoxLogicHandler.java
+++ pieces-bot/src/main/java/com/m1raynee/telegram/handlers/BoxLogicHandler.java
@@ -2,12 +2,8 @@ package com.m1raynee.telegram.handlers;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-
-import com.m1raynee.db.HibernateConfiguration;
-import com.m1raynee.db.dto.PieceAmountDTO;
+import com.m1raynee.db.Database;
 import com.m1raynee.db.entity.Box;
 import com.m1raynee.db.entity.Piece;
 
@@ -45,38 +41,7 @@ public class BoxLogicHandler {
             ctx.sendMessage(msg.chat.id, "Не найден номер коробки").exec();
             return;
         }
-        var sessionFactory = HibernateConfiguration.getSessionFactory();
-        Box box = sessionFactory.fromSession(session -> {
-            List<PieceAmountDTO> amounts = session.createSelectionQuery("""
-                    select new com.m1raynee.db.dto.PieceAmountDTO(p.id, SUM(pa.amount))
-                    from Piece p
-                    left join p.actions pa
-                    join p.box b
-                    where b.index=:index
-                    group by p.id
-                    """,
-                    PieceAmountDTO.class)
-                    .setParameter("index", box_index)
-                    .getResultList();
-
-            Map<Long, Long> stockMap = amounts.stream()
-                    .collect(Collectors.toMap(PieceAmountDTO::getPieceId,
-                            dto -> dto.getCurrentAmount() != null ? dto.getCurrentAmount() : 0L));
-
-            Box resultBox = session
-                    .createSelectionQuery("from Box b left join fetch b.pieces where b.index=:index", Box.class)
-                    .setParameter("index", box_index).getSingleResultOrNull();
-
-            for (Piece piece : resultBox.getPieces()) {
-                piece.setCalculatedAmount(stockMap.get(piece.getId()));
-            }
-            return resultBox;
-
-        });
-
-        System.out.println("Fetch complete");
-
-        sendBoxInformation(ctx, msg.chat.id, box);
+        sendBoxInformation(ctx, msg.chat.id, Database.fetchBoxPreview(box_index));
     }
 
     public void sendBoxInformation(BotContext ctx, Long chat_id, Box box) {
